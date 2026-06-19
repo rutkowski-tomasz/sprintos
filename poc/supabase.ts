de166d0cbf53b6a6d61f525208f71e525d6d085b
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2026-06-19 17:52:49 +0200

    refactor: remove not relevant POC code

--- poc/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://mxnwgddqwceohskvwnxm.supabase.co',
-  'sb_publishable_ZNAbAIv5Eex9vTeDNOhVSg_irh7oYem'
-)
